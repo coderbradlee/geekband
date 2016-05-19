26febbdc3d74e610ae9a04b97985296712726e19
lzxm160 <lzxm160@sina.com>
2016-05-19 10:29:45 +0800

    std

--- brad/week2/若明子/rectangle.cpp
+++ brad/week2/若明子/rectangle.cpp
@@ -1,61 +1,61 @@
 #include <memory>
 #include <iostream>
 #include "rectangle.h"
+using namespace std;
+Shape::Shape():no(0)
+{}
+Shape::Shape(int n):no(n)
+{} 
+int Shape::get_no()const
+{
+	return no;
+}
+       
 
-	Shape::Shape():no(0)
-	{}
-	Shape::Shape(int n):no(n)
-	{} 
-	int Shape::get_no()const
-	{
-		return no;
-	}
-           
-
-   int Point::get_x()const
-   {
-      return x;
-   }
-   int Point::get_y()const
-   {
-      return y;
-   }
-   Point::Point(int x,int y):x(x),y(y)
-   {
-   		cout<<"point constructor"<<endl;
-   }
-   Point::~Point()
-   {
-   		cout<<x<<":"<<y<<":point destructor"<<endl;
-   }
-             
-
-   Rectangle::Rectangle(int width, int height, int x, int y):width(width),height(height),leftUp(new Point(x,y)),Shape(10)
-	{
-
-	}
-   Rectangle::Rectangle(const Rectangle& other)
-   {
-   	width=other.width;
-	height=other.height;
-	leftUp=std::shared_ptr<Point>((new Point(other.leftUp->get_x(),other.leftUp->get_y())));
-   }
-   Rectangle& Rectangle::operator=(const Rectangle& other)
-   {
-		if(this==&other)
-			return *this;
-		width=other.width;
-		height=other.height;
-		cout<<"before reset"<<endl;
-		leftUp.reset(new Point(other.leftUp->get_x(),other.leftUp->get_y()));
-		cout<<"after reset"<<endl;
-		return *this;
-   }
+int Point::get_x()const
+{
+  return x;
+}
+int Point::get_y()const
+{
+  return y;
+}
+Point::Point(int x,int y):x(x),y(y)
+{
+		cout<<"point constructor"<<endl;
+}
+Point::~Point()
+{
+		cout<<x<<":"<<y<<":point destructor"<<endl;
+}
          
-   void Rectangle::print()
-   {
-   	cout<<width<<":"<<height<<":"<<leftUp->get_x()<<":"<<leftUp->get_y()<<":"<<get_no()<<endl;
-   }
+
+Rectangle::Rectangle(int width, int height, int x, int y):width(width),height(height),leftUp(new Point(x,y)),Shape(10)
+{
+
+}
+Rectangle::Rectangle(const Rectangle& other)
+{
+	width=other.width;
+height=other.height;
+leftUp=std::shared_ptr<Point>((new Point(other.leftUp->get_x(),other.leftUp->get_y())));
+}
+Rectangle& Rectangle::operator=(const Rectangle& other)
+{
+	if(this==&other)
+		return *this;
+	width=other.width;
+	height=other.height;
+	cout<<"before reset"<<endl;
+	leftUp.reset(new Point(other.leftUp->get_x(),other.leftUp->get_y()));
+	cout<<"after reset"<<endl;
+	return *this;
+}
+     
+void Rectangle::print()
+{
+	cout<<width<<":"<<height<<":"<<leftUp->get_x()<<":"<<leftUp->get_y()<<":"<<get_no()<<endl;
+}
 
 
 
